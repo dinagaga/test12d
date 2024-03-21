ec999f02aeced5f163c30c286f7f855f0e46c203
dinagaga <markanthonyespinosawewewawa@gmail.com>
2024-03-21 12:57:48 +0800

    first commit

---
+++ src/test12d/Test12d.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test12d;
+
+/**
+ *
+ * @author PC11
+ */
+public class Test12d {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
